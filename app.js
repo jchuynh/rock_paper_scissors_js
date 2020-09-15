fd43ec1a8bcd355bd9af23594e0ce35fa143d977
Jessica Huynh <chauhuynh.jessica@gmail.com>
2020-09-14 22:52:49 -0700

    Working error: classList null

--- app.js
+++ app.js
@@ -1,57 +1,83 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById('user-score'); // _span DOM variables
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector('.score-board');
-const result_div = document.querySelector('.result');
+const result_p = document.querySelector('.result > p');
 const rock_div = document.getElementById('rock');
 const paper_div = document.getElementById('paper');
 const scissors_div = document.getElementById('scissors');
 
 function getComputerChoice() {
-	const choices = ['rock', 'paper', 'scissors'];
+	const choices = ['Rock', 'Paper', 'Scissors'];
 	const randomNumber = Math.floor(Math.random() * 3);
 	return choices[randomNumber];
 }
+
+// function convertToWord(letter) {
+// 	if (letter === "r") return "Rock";
+// 	if (letter === "p") return "Paper";
+// 	if (letter === "s") return "Scissors";
+// }
 
 // console.log(getComputerChoice());
 
 // getComputerChoice();
 
+
+function win(user, computer) {
+	userScore++;
+	userScore_span.innerHTML = userScore;
+	computerScore_span.innerHTML = computerScore;
+	result_p.innerHTML = `${user} beats ${computer}. You Win!`;
+	document.getElementById(user).classList.add('green-glow');
+}
+
+function lose(user, computer) {
+	computerScore++;
+	userScore_span.innerHTML = userScore;
+	computerScore_span.innerHTML = computerScore;
+	result_p.innerHTML = `${computer} looses to ${user}. You Lose!`;
+}
+
+function draw(user, computer) {
+	userScore_span.innerHTML = userScore;
+	computerScore_span.innerHTML = computerScore;
+	result_p.innerHTML = `${computer} is the same as ${user}. Draw!`;
+}
+
 function game(userChoice) {
 	const computerChoice = getComputerChoice();
 	switch (userChoice + computerChoice) {
-		case 'rockscissors':
-		case 'paperrock':
-		case 'scissorspaper':
-			console.log('User Wins!');
+		case 'RockScissors':
+		case 'PaperRock':
+		case 'ScissorsPaper':
+			win(userChoice, computerChoice);
 			break;
-		case 'rockpaper':
-		case 'paperscissors':
-		case 'scissorsrock':
-			console.log('User loses!');
+		case 'RockPaper':
+		case 'PaperScissors':
+		case 'ScissorsRock':
+			lose(userChoice, computerChoice);
 			break;
-		case 'rockrock':
-		case 'paperpaper':
-		case 'scissorsscissors':
-		console.log('Draw!');
-		break;
+		case 'RockRock':
+		case 'PaperPaper':
+		case 'ScissorsScissors':
+			draw(userChoice, computerChoice);
+			break;
 	}
-	// console.log('userChoice ' + userChoice);
-	// console.log('compChoice ' + computerChoice);
 }
 
 function main() {
 	rock_div.addEventListener('click', function() {
-		game('rock');
+		game('Rock');
 	})
 
 	paper_div.addEventListener('click', function() {
-		game('paper');
+		game('Paper');
 	})
 
 	scissors_div.addEventListener('click', function() {
-		game('scissors');
+		game('Scissors');
 	})
 }
 
